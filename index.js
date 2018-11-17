16a327983ff92d2643848bbac3450685c1e74436
Stell <stell121314@gmail.com>
2018-11-17 21:06:10 +0700

    render by pug

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index', {name: 'Phu'})
 });
 app.listen(port, () => 
     console.log(`Example app listening on port ${port}`));

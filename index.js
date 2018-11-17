4b2ca38d3fb80bf675385d59684ddb41baadde1f
Stell <stell121314@gmail.com>
2018-11-17 21:10:39 +0700

    render users list

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+    {id: 1, name: 'Phu'},
+    {id: 2, name: 'Huy'}
+];
 app.get('/', (req, res) => {
     res.render('index', {name: 'Phu'})
 });
+app.get('/users', (req, res) => {
+    res.render('users/index', {
+        user: users
+    })
+});
+
+
 app.listen(port, () => 
     console.log(`Example app listening on port ${port}`));

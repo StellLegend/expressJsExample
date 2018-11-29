fa0234770a3b24a9090b5f6fec859a85b0627b7a
Stell <stell121314@gmail.com>
2018-11-29 17:34:14 +0700

    Upload file using multer

--- route/user.route.js
+++ route/user.route.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var multer  = require('multer');
+
+var upload = multer({ dest: './public/uploads' });//add folder
+
 var router = express();
+
 
 var bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 const controller = require('../controller/user.controller');
@@ -16,6 +22,10 @@ router.get('/', authMiddleware.loginAuth, controller.index);
 router.get('/search', controller.search);
 router.get('/create', controller.create);
 router.get('/:id', controller.getID);
-router.post('/create', userMiddleware.postCreate, controller.postCreate);
+router.post('/create', 
+    upload.single('avatar'), 
+    userMiddleware.postCreate, 
+    controller.postCreate
+);
 
 module.exports = router;

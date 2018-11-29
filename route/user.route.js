c84d2fa804bf93c92f841645be05840d7977ee6c
Stell <stell121314@gmail.com>
2018-11-29 21:28:16 +0700

    update

--- route/user.route.js
+++ route/user.route.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var multer  = require('multer');
 
-var upload = multer({ dest: './public/uploads' });//add folder
+var upload = multer({ dest: './public/uploads/' });//add folder
 
 var router = express();
 
